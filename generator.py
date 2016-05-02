cc4d6edc8395d87f59cbd2133f71d3169007df71
Rob <telamascope@gmail.com>
2016-05-02 02:10:18 -0400

    added Eve as identity

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import subprocess
 from os import walk, remove
 from os.path import join
 
-def gen_voters():
+def gen_voters(unreg=False):
     n = int(input("Enter how many registered voters to generate\n"))
     
     first_names = []
@@ -26,8 +26,11 @@ def gen_voters():
         last_names.append(temp)
     fp.close()
 
+    if unreg:
+        people.append((first_names[0], last_names[0]))
+        return people
+    
     fp = open('reg_voters.txt', 'w')
-
     i = 0
     while i < n:
         temp = (random.choice(first_names), random.choice(last_names))
@@ -39,12 +42,15 @@ def gen_voters():
     return people
     
 
-def gen_keys(voters):
+def gen_keys(voters, unreg=False):
     i = 0
     dr = 'auth/voters'
     
     for person in voters:
-        outfile = join(dr, "key-" + str(i) + ".pem")
+        if unreg:
+            outfile = join(dr, "unreg-key-" + str(i) + ".pem")
+        else:
+            outfile = join(dr, "key-" + str(i) + ".pem")
         subprocess.check_call(["openssl", "genpkey", "-algorithm", "RSA", "-out", outfile])
         i+= 1
 
@@ -54,17 +60,35 @@ def del_voters():
         for name in files:
             remove(join(root,name))
 
-def gen_certs(voters):
+def gen_certs(voters, eve=False, unreg=False):
     dr = 'auth'
-    ca_cert = join(dr, 'ca-cert.pem')
-    ca_key = join(dr, 'ca-key.pem')
-    ca_serial = join(dr, 'ca-cert.srl')
+    if eve:
+        ca_cert = join(dr, 'eve-cert.pem')
+        ca_key = join(dr, 'eve-key.pem')
+        ca_serial = join(dr, 'eve-cert.srl')
+    else:
+        ca_cert = join(dr, 'ca-cert.pem')
+        ca_key = join(dr, 'ca-key.pem')
+        ca_serial = join(dr, 'ca-cert.srl')
     i = 0
 
+    k = 'key-'
+    cs = 'csr'
+    ce = 'cert'
+    
+    if unreg:
+        key = 'unreg-' +  k
+        csr = 'unreg-' + cs
+        cert = 'unreg-' +  ce
+    else:
+        key = k
+        csr = cs
+        cert = ce
+
     for people in voters:
-        v_key = join(dr, 'voters', 'key-' + str(i) + '.pem')
-        v_csr = join(dr, 'voters', 'csr-' + str(i) + '.pem')
-        v_cert = join(dr, 'voters', 'cert-' + str(i) + '.pem')
+        v_key = join(dr, 'voters', key + str(i) + '.pem')
+        v_csr = join(dr, 'voters', csr + str(i) + '.pem')
+        v_cert = join(dr, 'voters', cert + str(i) + '.pem')
         
         csr_opt = "/CN=" + people[1] + ", " + people[0]
         
@@ -82,19 +106,3 @@ def gen_certs(voters):
                 "-out", v_cert])
         remove(v_csr)
         i+=1
-
-def sign_csrs():
-    dr = 'auth/voters'
-    ca_cert = 'auth/ca-cert.pem'
-    ca_key = 'auth/ca-key.pem'
-
-    for root, dirs, files in walk(dr):
-        for name in files:
-            if 'csr' in name:
-                subprocess.check_call(
-                        ["openssl", "x509", "-req", 
-                        "-CA", ca_cert,
-                        "-CAkey", ca_key, 
-                        "-CAcreateserial",
-                        "-in", join(root, name), 
-                        "-out", join(root, 'cert' + name[3:])])
